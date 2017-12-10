af4a16defdc312ca50e6018ca529f07d5f18aafe
ROBO-LAPTOP\Raz <Raz@yosigal.com>
2017-12-10 20:31:22 +0200

    Added mechanism to change hosts easily Added some constants

--- plot_arduino.py
+++ plot_arduino.py
@@ -1,5 +1,20 @@
 import serial
-device = "/dev/tty.wchusbserial1420"
-ser = serial.Serial(device, 9600)
+
+# ENVIRONMENT
+SHIMMY = "SHIMMY"
+RAZ = "RAZ"
+HOSTS = {RAZ, SHIMMY}
+HOST2DEV = {SHIMMY : "/dev/tty.wchusbserial1420",
+            RAZ : "NO DEVICE"}
+DEFAULT_BAUD_RATE = 9600
+
+# CURRENT HOST #
+# ============ #
+HOST = SHIMMY  #
+# ============ #
+
+device = HOST2DEV[HOST]
+
+ser = serial.Serial(device, DEFAULT_BAUD_RATE)
 while True:
     print(str(ser.readline()))
