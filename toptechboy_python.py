1a5dbcf1739ef46add5d49c7105aa327811def85
ROBO-LAPTOP\Raz <Raz@yosigal.com>
2017-12-17 09:29:07 +0200

    added toptechboy code

---
+++ toptechboy_python.py
@@ -0,0 +1,10 @@
+import serial  # Import Serial Library
+
+arduinoSerialData = serial.Serial('COM5',
+                                  9600)  # Create Serial port object called
+# arduinoSerialData
+
+while (1 == 1):
+    if (arduinoSerialData.inWaiting() > 0):
+        myData = arduinoSerialData.readline()
+        print(myData)
